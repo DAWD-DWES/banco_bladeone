58297ebf6d227503ec6f35f4eba910da3e4aa487
jivanrod <jivanrod@gmail.com>
2025-02-10 19:37:00 +0100

    fix

--- src/modelo/Banco.php
+++ src/modelo/Banco.php
@@ -149,9 +149,8 @@ class Banco {
      * @param string $nombre Nombre del banco
      * @return $this
      */
-    public function setNombre(string $nombre) {
+    public function setNombre(string $nombre): void {
         $this->nombre = $nombre;
-        return $this;
     }
 
     /**
@@ -160,7 +159,7 @@ class Banco {
      * @param float $comisionCC Comisión del banco
      * @return $this
      */
-    public function setComisionCC(float $comisionCC) {
+    public function setComisionCC(float $comisionCC): void {
         $this->comisionCC = $comisionCC;
     }
 
@@ -170,7 +169,7 @@ class Banco {
      * @param float $minSaldoComisionCC mínimo saldo sin comisión
      * @return $this
      */
-    public function setMinSaldoComisionCC(float $minSaldoComisionCC) {
+    public function setMinSaldoComisionCC(float $minSaldoComisionCC): void {
         $this->minSaldoComisionCC = $minSaldoComisionCC;
     }
 
@@ -180,7 +179,7 @@ class Banco {
      * @param float $interesCA Interés del banco
      * @return $this
      */
-    public function setInteresCA(float $interesCA) {
+    public function setInteresCA(float $interesCA): void {
         $this->interesCA = $interesCA;
     }
 
@@ -379,7 +378,7 @@ class Banco {
     /**
      * Aplica cargos de comisión a la cuenta corriente
      */
-    public function aplicaComisionCC() {
+    public function aplicaComisionCC(): void {
         $cuentasCorrientes = array_filter($this->obtenerCuentas(), fn($cuenta) => $cuenta instanceof CuentaCorriente);
 
 // Captura las propiedades necesarias con 'use'
@@ -391,7 +390,7 @@ class Banco {
         });
     }
 
-    public function aplicaInteresCA() {
+    public function aplicaInteresCA(): void {
         $cuentasAhorros = array_filter($this->obtenerCuentas(), fn($cuenta) => $cuenta instanceof CuentaAhorros);
 
 // Captura las propiedades necesarias con 'use'
